fbd045509b754170924209b26916986c1606a015
Riaz Virani <riaz.n.virani@gmail.com>
2016-03-03 13:42:38 -0500

    Don't ajax contact form

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -168,34 +168,5 @@ jQuery(document).ready(function () {
   $('.contact-form form input[type="text"], .contact-form form textarea').on('focus', function () {
     $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
   });
-  $('.contact-form form').submit(function (e) {
-    e.preventDefault();
-    $('.contact-form form input[type="text"], .contact-form form textarea').removeClass('contact-error');
-    var postdata = $('.contact-form form').serialize();
-    $.ajax({
-      type: 'POST',
-      url: '//formspree.io/riaz@monokaicode.co',
-      data: postdata,
-      dataType: 'json',
-      success: function (json) {
-        if (json.emailMessage != '') {
-          $('.contact-form form .contact-email').addClass('contact-error');
-        }
-        if (json.subjectMessage != '') {
-          $('.contact-form form .contact-subject').addClass('contact-error');
-        }
-        if (json.messageMessage != '') {
-          $('.contact-form form textarea').addClass('contact-error');
-        }
-        if (json.emailMessage == '' && json.subjectMessage == '' && json.messageMessage == '') {
-          $('.contact-form form').fadeOut('fast', function () {
-            $('.contact-form').append('<p>Thanks for contacting us! We will get back to you very soon.</p>');
-          });
-        }
-      }
-    });
-  });
-
-
 });
 
